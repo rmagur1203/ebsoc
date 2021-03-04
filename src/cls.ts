a8fc659e02dd13094fb3cefff1ee65965028a45a
rmagur203 <41236839+rmagur1203@users.noreply.github.com>
2021-03-04 21:07:02 +0900

    update & create functions

--- src/cls.ts
+++ src/cls.ts
@@ -3,7 +3,7 @@ import path from './path.json';
 
 export let mypage = {
     myClassListByTabType: async function (token: string, data: { orderType?: string, schoolAffairsYear: number, searchType: string, searchWord: string, tabType: string }) {
-        let url: string = "https://" + path.host + path.common.member;
+        let url: string = "https://" + path.host + path.cls.mypage.myClassListByTabType;
         let req = await axios({
             method: 'POST',
             url: url,
@@ -19,9 +19,9 @@ export let mypage = {
 
 export let cmyNote = {
     receiptNotOpenCount: async function (token: string) {
-        let url: string = "https://" + path.host + path.common.member;
+        let url: string = "https://" + path.host + path.cls.cmyNote.receiptNotOpenCount;
         let req = await axios({
-            method: 'POST',
+            method: 'GET',
             url: url,
             headers: {
                 "X-AUTH-TOKEN": token,
@@ -34,7 +34,7 @@ export let cmyNote = {
 
 export let communityChattingRoom = {
     communityMyChattingParticipationListCnt: async function (token: string) {
-        let url: string = "https://" + path.host + path.common.member;
+        let url: string = "https://" + path.host + path.cls.communityChattingRoom.communityMyChattingParticipationListCnt;
         let req = await axios({
             method: 'POST',
             url: url,
@@ -47,3 +47,19 @@ export let communityChattingRoom = {
         return req.data;
     }
 }
+
+export let school = {
+    schoolClassList: async function (token: string, data: { classSeCd: string, offecCd: string, orderType: string, schlGrdCd: string, schoolAffairsYear: string, schoolCode: string, searchWord: string, userSequenceNo: number }) {
+        let url: string = "https://" + path.host + path.cls.school.schoolClassList;
+        let req = await axios({
+            method: 'POST',
+            url: url,
+            headers: {
+                "X-AUTH-TOKEN": token,
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Safari/537.36"
+            },
+            data: data
+        });
+        return req.data;
+    }
+};
--- src/common.ts
+++ src/common.ts
@@ -4,7 +4,7 @@ import path from './path.json';
 export async function member(token: string) {
     let url: string = "https://" + path.host + path.common.member;
     let req = await axios({
-        method: 'POST',
+        method: 'GET',
         url: url,
         headers: {
             "X-AUTH-TOKEN": token,
@@ -17,7 +17,7 @@ export async function member(token: string) {
 export async function noticeCommonMainList(token: string) {
     let url: string = "https://" + path.host + path.common.noticeCommonMainList;
     let req = await axios({
-        method: 'POST',
+        method: 'GET',
         url: url,
         headers: {
             "X-AUTH-TOKEN": token,
@@ -30,7 +30,7 @@ export async function noticeCommonMainList(token: string) {
 export async function noticeSchoolMainList(token: string) {
     let url: string = "https://" + path.host + path.common.noticeSchoolMainList;
     let req = await axios({
-        method: 'POST',
+        method: 'GET',
         url: url,
         headers: {
             "X-AUTH-TOKEN": token,
@@ -39,3 +39,18 @@ export async function noticeSchoolMainList(token: string) {
     });
     return req.data;
 }
+
+export let school = {
+    info: async function (token: string, schoolCode: string) {
+        let url: string = "https://" + path.host + path.common.school.info.replace('${schoolCode}', schoolCode);
+        let req = await axios({
+            method: 'GET',
+            url: url,
+            headers: {
+                "X-AUTH-TOKEN": token,
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Safari/537.36"
+            }
+        });
+        return req.data;
+    }
+};
