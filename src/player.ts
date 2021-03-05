ecd47e31600a8bbc9d91465403370b2f01865aab
rmagur203 <41236839+rmagur1203@users.noreply.github.com>
2021-03-05 20:49:08 +0900

    update

--- src/player.ts
+++ src/player.ts
@@ -5,14 +5,26 @@ import { lessonDetail } from './common';
 
 import Path from './path.json';
 
-export let currentDuration = 0;
-
 const progressIntervalInSeconds = 30;
 export { progressIntervalInSeconds };
 
 const key = CryptoJS.enc.Latin1.parse('l40jsfljasln32uf');
 const iv = CryptoJS.enc.Latin1.parse('asjfknal3bafjl23');
 export { key, iv };
+
+type fieldType = {
+    token: string,
+    data: {
+        memberSeq: number,
+        lctreLrnSqno: number,
+        lessonSeq: number
+    },
+    video: {
+        url: string,
+        duration: number
+    },
+    current: number
+};
 
 export function encrypt(memberSeq: number, lctreLrnSqno: number, progressRate: number) {
     var data = `${memberSeq}|${lctreLrnSqno}|${progressRate}`;
@@ -69,33 +81,26 @@ export async function getCurrentDuration(token: string, video: { url: string, du
     return video.duration * current;
 }
 
-export function intervalCallback(token: string, data: { memberSeq: number, lctreLrnSqno: number, currentDuration: number, playTime: number }) {
-    callApi(token, {
-        memberSeq: data.memberSeq,
-        lctreLrnSqno: data.lctreLrnSqno,
-        rate: makeRate(data.currentDuration, data.playTime)
+export function intervalCallback(field: fieldType) {
+    callApi(field.token, {
+        memberSeq: field.data.memberSeq,
+        lctreLrnSqno: field.data.lctreLrnSqno,
+        rate: makeRate(field.current, field.video.duration)
     });
-    currentDuration += progressIntervalInSeconds;
+    field.current += progressIntervalInSeconds;
 
     /* Use this when debugging
-    console.log(`[DEBUG] currentDuration: ${currentDuration}`);
+     * console.log(`[DEBUG] currentDuration: ${currentDuration}`);
      */
+}
+
+export function pauseCallback(field: fieldType) {
+
 }
 
 export default class Player {
     timer: number = NaN;
-    fields: {
-        token: string,
-        data: {
-            memberSeq: number,
-            lctreLrnSqno: number,
-            lessonSeq: number
-        },
-        video: {
-            url: string,
-            duration: number
-        }
-    };
+    fields: fieldType;
     clearIntvl: number = NaN;
     constructor(token: string, options: { memberSeq: number, lctreLrnSqno: number, lessonSeq: number, videoUrl: string }) {
         this.fields = {
@@ -108,14 +113,15 @@ export default class Player {
             video: {
                 url: options.videoUrl,
                 duration: NaN
-            }
+            },
+            current: 0
         };
         this.getDuration(token, options.lessonSeq);
     }
     async getDuration(token: string, lessonSeq: number) {
         const duration = await getTotalDuration(token, lessonSeq);
         this.fields.video.duration = duration;
-        currentDuration = await getCurrentDuration(token, duration);
+        this.fields.current = await getCurrentDuration(token, duration);
     }
     play() {
         if (Number.isNaN(this.timer)) {
--- src/test.ts
+++ src/test.ts
@@ -6,7 +6,12 @@ import Player, { encrypt } from "./player";
 (async () => {
     let data = await login("rmagur12032", "password");
     let token: string = data.data.token;
-    data = await member(token);
+    let player = new Player(token, {
+        memberSeq: 6915982,
+        lctreLrnSqno: 4738,
+        lessonSeq: 1933,
+        videoUrl: 'https://sel2.ebsoc.co.kr/class/sunrinkorean1/course/1903/lecture/554904'
+    });
     /*
     let player = new Player({
         token: token,
